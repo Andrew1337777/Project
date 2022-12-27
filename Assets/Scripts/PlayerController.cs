1678b8e4d6fa9af9de1d19d02ebc080dac0bb458
ArsGrud8 <ars.grud@gmail.com>
2022-12-27 15:25:49 +1000

    Start

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,45 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+
 public class PlayerController : MonoBehaviour
 {
-
-    public Text scoreText;
-    public float score;
     public float speed;
     public float jumpForce;
+    public int score;
+    public Text scoreText;
+    public GameObject spawn;
 
+    public float timeScale;
+    public float timeScaleMax;
 
-    private GameObject spawn;
-    private Rigidbody2D rigidbody2d;
     private bool isGround;
+    private Rigidbody2D rigidbody2D;
     private SpriteRenderer spriteRenderer;
-
+    private Animator animator;
     private void Start()
     {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        scoreText.text = score.ToString();
+
+
         spawn = GameObject.FindWithTag("Respawn");
         transform.position = spawn.transform.position;
-        rigidbody2d = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-
     }
-
-
-
-    private void Update()
+    void Update()
     {
+        BonusCheck();
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+
             SceneManager.LoadScene(0);
+
         }
-
-
         Vector3 position = transform.position;
-
         position.x += Input.GetAxis("Horizontal") * speed;
+
 
         transform.position = position;
 
@@ -49,9 +54,42 @@ public class PlayerController : MonoBehaviour
             Jump();
         }
     }
+
+    public void ScaleBonus()
+    {
+        timeScale = timeScaleMax;
+    }
+
+    private void BonusCheck()
+    {
+        if (timeScale > 0)
+        {
+            transform.localScale = new Vector3(1.5f, 1.5f, 1f);
+            timeScale--;
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+
+
+        }
+    }
+
+
+    public void AddCoin(int count)
+    {
+        score += count;
+        scoreText.text = score.ToString();
+
+
+
+    }
+
     private void Jump()
     {
-        rigidbody2d.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+
+        rigidbody2D.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
